8c05a67be99a19140b20f689d8d5e711976b6028
Abhiram2006 <cherukupalliabhiram@gmail.com>
2025-03-22 01:38:59 -0700

    first attempt at SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://abhiram2006.github.io/website/',
+  site: 'https://abhiram2006.github.io/website/', // Full production URL
   base: '/website/',
   outDir: './docs',
+  integrations: [sitemap()],
 });
